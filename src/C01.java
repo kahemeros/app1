ea870c3972c2a0b78668be1ab37c5e1f2b7b93d0
Erdal Altuncu <kahemeros@gmail.com>
2022-08-17 12:35:41 +0300

    v3

--- src/C01.java
+++ src/C01.java
@@ -2,6 +2,8 @@ public class C01 {
     public static void main(String[] args) {
         System.out.println("Deneme 01");
         System.out.println("Deneme 02");
+        System.out.println("Deneme 03");
+
 
     }
 }
