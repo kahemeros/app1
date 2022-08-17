a158849d9347615953ba26de9dae20a28153a456
Erdal Altuncu <kahemeros@gmail.com>
2022-08-17 18:27:00 +0300

    v3 aktive edildi

--- src/C01.java
+++ src/C01.java
@@ -3,7 +3,6 @@ public class C01 {
         System.out.println("Deneme 01");
         System.out.println("Deneme 02");
         System.out.println("Deneme 03");
-        System.out.println("Deneme 05");
 
 
     }
