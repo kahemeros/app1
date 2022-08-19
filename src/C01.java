b31c0c10fbe18e12e029e172d555b656ccb66f44
kahemeros <110896112+kahemeros@users.noreply.github.com>
2022-08-19 12:01:21 +0300

    v6

--- src/C01.java
+++ src/C01.java
@@ -5,6 +5,7 @@ public class C01 {
         System.out.println("Deneme 03");
         System.out.println("Deneme 05");
         System.out.println("erdal branch'inde ilk ekleme");
+        System.out.println("github'dan push yapma deneyimi");
 
 
     }
